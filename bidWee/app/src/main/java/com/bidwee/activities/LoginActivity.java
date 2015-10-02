44be48d44532e24a19b457e35bb750c43af8cf15
Suneet Agrawal <agrawalsuneet@gmail.com>
2015-10-02 13:37:58 +0530

    Login Screen Implemented

--- bidWee/app/src/main/java/com/bidwee/activities/LoginActivity.java
+++ bidWee/app/src/main/java/com/bidwee/activities/LoginActivity.java
@@ -2,8 +2,18 @@ package com.bidwee.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView loginAsCustomerTextView, loginAsMerchantTextView,
+                forgotPasswordTextView;
+
+    private EditText usernameEditText, passwordEditText;
+    private Button registerButton, loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +21,44 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         getSupportActionBar().hide();
+
+        initControls();
+    }
+
+    private void initControls() {
+        loginAsCustomerTextView = (TextView)findViewById(R.id.login_asCustomerTextview);
+        loginAsMerchantTextView = (TextView)findViewById(R.id.login_asMerchantTextview);
+        forgotPasswordTextView = (TextView)findViewById(R.id.login_forgotParrwordTextview);
+
+        usernameEditText = (EditText)findViewById(R.id.login_userNameEditText);
+        passwordEditText = (EditText)findViewById(R.id.login_passwordEditText);
+
+        registerButton = (Button)findViewById(R.id.login_registerButton);
+        loginButton = (Button)findViewById(R.id.login_loginButton);
+
+        loginAsCustomerTextView.setOnClickListener(this);
+        loginAsMerchantTextView.setOnClickListener(this);
+        forgotPasswordTextView.setOnClickListener(this);
+
+        registerButton.setOnClickListener(this);
+        loginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.login_asCustomerTextview :
+                loginAsCustomerTextView.setTextColor(getResources().getColor(R.color.login_selectedtext_color));
+                loginAsMerchantTextView.setTextColor(getResources().getColor(R.color.white_color));
+                break;
+
+            case R.id.login_asMerchantTextview :
+                loginAsCustomerTextView.setTextColor(getResources().getColor(R.color.white_color));
+                loginAsMerchantTextView.setTextColor(getResources().getColor(R.color.login_selectedtext_color));
+                break;
+
+            default:
+                break;
+        }
     }
 }
